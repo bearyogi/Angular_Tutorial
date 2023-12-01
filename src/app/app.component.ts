f5886ad93643692e54251c3da817f3e90f70ab75
Michał Mamla <mmichal1999@gmail.com>
2023-12-01 20:52:53 +0100

    Lessons 10 - 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,18 @@ import { RouterModule } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HomeComponent, RouterModule],
-  templateUrl: './app.component.html',
+template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 
